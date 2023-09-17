c29bfdc71f1f45484df5323dd4603e74049e8456
luanhailiang <hi@luanhailiang.cn>
2023-09-17 16:17:25 +0800

    Update csv_example_test.go

--- csv_example_test.go
+++ csv_example_test.go
@@ -9,137 +9,137 @@ import (
 	"log"
 )
 
-func Example_readStruct() {
-	fname := "testdata/simple.csv"
-	tbl, err := Open(fname)
-	if err != nil {
-		log.Fatalf("could not open %s: %v\n", fname, err)
-	}
-	defer tbl.Close()
-	tbl.Reader.Comma = ';'
-	tbl.Reader.Comment = '#'
+// func Example_readStruct() {
+// 	fname := "testdata/simple.csv"
+// 	tbl, err := Open(fname)
+// 	if err != nil {
+// 		log.Fatalf("could not open %s: %v\n", fname, err)
+// 	}
+// 	defer tbl.Close()
+// 	tbl.Reader.Comma = ';'
+// 	tbl.Reader.Comment = '#'
 
-	rows, err := tbl.ReadRows(0, 10)
-	if err != nil {
-		log.Fatalf("could read rows [0, 10): %v\n", err)
-	}
-	defer rows.Close()
+// 	rows, err := tbl.ReadRows(0, 10)
+// 	if err != nil {
+// 		log.Fatalf("could read rows [0, 10): %v\n", err)
+// 	}
+// 	defer rows.Close()
 
-	irow := 0
-	for rows.Next() {
-		data := struct {
-			I int
-			F float64
-			S string
-		}{}
-		err = rows.Scan(&data)
-		if err != nil {
-			log.Fatalf("error reading row %d: %v\n", irow, err)
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatalf("error: %v\n", err)
-	}
-}
+// 	irow := 0
+// 	for rows.Next() {
+// 		data := struct {
+// 			I int
+// 			F float64
+// 			S string
+// 		}{}
+// 		err = rows.Scan(&data)
+// 		if err != nil {
+// 			log.Fatalf("error reading row %d: %v\n", irow, err)
+// 		}
+// 	}
+// 	err = rows.Err()
+// 	if err != nil {
+// 		log.Fatalf("error: %v\n", err)
+// 	}
+// }
 
-func Example_readSlice() {
-	fname := "testdata/simple.csv"
-	tbl, err := Open(fname)
-	if err != nil {
-		log.Fatalf("could not open %s: %v\n", fname, err)
-	}
-	defer tbl.Close()
-	tbl.Reader.Comma = ';'
-	tbl.Reader.Comment = '#'
+// func Example_readSlice() {
+// 	fname := "testdata/simple.csv"
+// 	tbl, err := Open(fname)
+// 	if err != nil {
+// 		log.Fatalf("could not open %s: %v\n", fname, err)
+// 	}
+// 	defer tbl.Close()
+// 	tbl.Reader.Comma = ';'
+// 	tbl.Reader.Comment = '#'
 
-	rows, err := tbl.ReadRows(0, 10)
-	if err != nil {
-		log.Fatalf("could read rows [0, 10): %v\n", err)
-	}
-	defer rows.Close()
+// 	rows, err := tbl.ReadRows(0, 10)
+// 	if err != nil {
+// 		log.Fatalf("could read rows [0, 10): %v\n", err)
+// 	}
+// 	defer rows.Close()
 
-	irow := 0
-	for rows.Next() {
-		var (
-			I int
-			F float64
-			S string
-		)
-		err = rows.Scan(&I, &F, &S)
-		if err != nil {
-			log.Fatalf("error reading row %d: %v\n", irow, err)
-		}
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatalf("error: %v\n", err)
-	}
-}
+// 	irow := 0
+// 	for rows.Next() {
+// 		var (
+// 			I int
+// 			F float64
+// 			S string
+// 		)
+// 		err = rows.Scan(&I, &F, &S)
+// 		if err != nil {
+// 			log.Fatalf("error reading row %d: %v\n", irow, err)
+// 		}
+// 	}
+// 	err = rows.Err()
+// 	if err != nil {
+// 		log.Fatalf("error: %v\n", err)
+// 	}
+// }
 
-func Example_writeStruct() {
-	fname := "out.csv"
-	tbl, err := Create(fname)
-	if err != nil {
-		log.Fatalf("could not create %s: %v\n", fname, err)
-	}
-	defer tbl.Close()
-	tbl.Writer.Comma = ';'
+// func Example_writeStruct() {
+// 	fname := "out.csv"
+// 	tbl, err := Create(fname)
+// 	if err != nil {
+// 		log.Fatalf("could not create %s: %v\n", fname, err)
+// 	}
+// 	defer tbl.Close()
+// 	tbl.Writer.Comma = ';'
 
-	err = tbl.WriteHeader("## a simple set of data: int64;float64;string\n")
-	if err != nil {
-		log.Fatalf("error writing header: %v\n", err)
-	}
+// 	err = tbl.WriteHeader("## a simple set of data: int64;float64;string\n")
+// 	if err != nil {
+// 		log.Fatalf("error writing header: %v\n", err)
+// 	}
 
-	for i := 0; i < 10; i++ {
-		data := struct {
-			I int
-			F float64
-			S string
-		}{
-			I: i,
-			F: float64(i),
-			S: fmt.Sprintf("str-%d", i),
-		}
-		err = tbl.WriteRow(data)
-		if err != nil {
-			log.Fatalf("error writing row %d: %v\n", i, err)
-		}
-	}
+// 	for i := 0; i < 10; i++ {
+// 		data := struct {
+// 			I int
+// 			F float64
+// 			S string
+// 		}{
+// 			I: i,
+// 			F: float64(i),
+// 			S: fmt.Sprintf("str-%d", i),
+// 		}
+// 		err = tbl.WriteRow(data)
+// 		if err != nil {
+// 			log.Fatalf("error writing row %d: %v\n", i, err)
+// 		}
+// 	}
 
-	err = tbl.Close()
-	if err != nil {
-		log.Fatalf("error closing table: %v\n", err)
-	}
-}
+// 	err = tbl.Close()
+// 	if err != nil {
+// 		log.Fatalf("error closing table: %v\n", err)
+// 	}
+// }
 
-func Example_writeSlice() {
-	fname := "out.csv"
-	tbl, err := Create(fname)
-	if err != nil {
-		log.Fatalf("could not create %s: %v\n", fname, err)
-	}
-	defer tbl.Close()
-	tbl.Writer.Comma = ';'
+// func Example_writeSlice() {
+// 	fname := "out.csv"
+// 	tbl, err := Create(fname)
+// 	if err != nil {
+// 		log.Fatalf("could not create %s: %v\n", fname, err)
+// 	}
+// 	defer tbl.Close()
+// 	tbl.Writer.Comma = ';'
 
-	err = tbl.WriteHeader("## a simple set of data: int64;float64;string\n")
-	if err != nil {
-		log.Fatalf("error writing header: %v\n", err)
-	}
+// 	err = tbl.WriteHeader("## a simple set of data: int64;float64;string\n")
+// 	if err != nil {
+// 		log.Fatalf("error writing header: %v\n", err)
+// 	}
 
-	for i := 0; i < 10; i++ {
-		var (
-			f = float64(i)
-			s = fmt.Sprintf("str-%d", i)
-		)
-		err = tbl.WriteRow(i, f, s)
-		if err != nil {
-			log.Fatalf("error writing row %d: %v\n", i, err)
-		}
-	}
+// 	for i := 0; i < 10; i++ {
+// 		var (
+// 			f = float64(i)
+// 			s = fmt.Sprintf("str-%d", i)
+// 		)
+// 		err = tbl.WriteRow(i, f, s)
+// 		if err != nil {
+// 			log.Fatalf("error writing row %d: %v\n", i, err)
+// 		}
+// 	}
 
-	err = tbl.Close()
-	if err != nil {
-		log.Fatalf("error closing table: %v\n", err)
-	}
-}
+// 	err = tbl.Close()
+// 	if err != nil {
+// 		log.Fatalf("error closing table: %v\n", err)
+// 	}
+// }
